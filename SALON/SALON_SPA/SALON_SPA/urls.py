83715ce9dc211438e46c4157f7fb2dfe40b95ff4
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-03-24 23:38:57 +0300

    product app

--- SALON/SALON_SPA/SALON_SPA/urls.py
+++ SALON/SALON_SPA/SALON_SPA/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('api/', include('api.urls')),
+    path('product/', include('products.urls')),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='passwords/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="passwords/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='passwords/password_reset_complete.html'), name='password_reset_complete'),
--- SALON/SALON_SPA/products/views.py
+++ SALON/SALON_SPA/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def products(request):
+    return render(request, 'products.html')
+
+def cart(request):
+    return render(request, 'cart.html')
