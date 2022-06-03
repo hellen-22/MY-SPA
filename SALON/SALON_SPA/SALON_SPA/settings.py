9e0eba92b826709b875596cc29693793af497634
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-06-03 14:52:34 +0300

    customizing messages

--- SALON/SALON_SPA/SALON_SPA/settings.py
+++ SALON/SALON_SPA/SALON_SPA/settings.py
@@ -78,6 +78,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SALON_SPA.wsgi.application'
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
