81b8fe90e84920defb400efb2c29d0156c80007f
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-09-23 13:58:27 +0300

    adding some models and custom admin page

--- SALON/SALON_SPA/products/admin.py
+++ SALON/SALON_SPA/products/admin.py
@@ -1,6 +1,28 @@
+from re import A
 from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['']
+    list_display = ['name', 'category', 'price']
+
 admin.site.register(Service)
+admin.site.register(Categories)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    search_fields = ['']
+    autocomplete_fields = ['customer']
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['order', 'product']
+    list_display = ['product', 'quantity', 'price', 'total']
+
+    @admin.display(ordering='price')
+    def total(self, orderItem):
+       return orderItem.quantity * orderItem.price
--- SALON/SALON_SPA/products/models.py
+++ SALON/SALON_SPA/products/models.py
@@ -1,10 +1,11 @@
-from statistics import mode
 from django.db import models
-from account.models import *
-from datetime import datetime
+
 
 class Categories(models.Model):
     category = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.category
 
 
 class Product(models.Model):
@@ -12,16 +13,47 @@ class Product(models.Model):
     name = models.CharField(max_length=20, verbose_name='name')
     description = models.TextField(max_length=200, verbose_name='description')
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    price = models.IntegerField(verbose_name='price')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.IntegerField()
 
     def __str__(self):
-        return str(self.category)
+        return str(self.name)
 
 class Service(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=200)
-    price = models.IntegerField(verbose_name='service_price')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    PAYMENT_CHOICES = [
+        ('C', 'Completed'),
+        ('P', 'Pending'),
+        ('F', 'Failed')
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=255, choices=PAYMENT_CHOICES)
+    customer = models.ForeignKey('account.CustomUser', on_delete=models.PROTECT)
+
+
+    def __str__(self) -> str:
+        return str(self.customer)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self) -> str:
+        return str(self.order)
+
+
+
+
+
+
+
